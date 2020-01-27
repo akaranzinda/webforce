b5f2b45033163a02f9dd588be6ba3d9d1f2568f6
alexis kara <akaranzinda@yahoo.fr>
2020-01-27 15:59:40 +0100

    ajout login et une partie du register

---
+++ src/Entity/User.php
@@ -0,0 +1,79 @@
+<?php
+// src/Entity/User.php
+
+namespace App\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ * @UniqueEntity(fields={"username"}, message="There is already an account with this username")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     */
+    private $prenom;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+     /**
+     * Get the value of nom
+     */ 
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set the value of nom
+     *
+     * @return  self
+     */ 
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of prenom
+     */ 
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set the value of prenom
+     *
+     * @return  self
+     */ 
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+}
